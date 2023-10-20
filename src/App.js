721f0ca01afd816b361614eeae4a44938543f8d0
Aquib <shaikmohammedaquib@gmail.com>
2023-10-20 20:34:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import About from "./components/About";
+//import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-import {
+/*import {
   BrowserRouter as Router,
   Route,
   Routes, // Import Routes from react-router-dom
-} from "react-router-dom";
+} from "react-router-dom";*/
 
 function App() {
   const [mode, setMode] = useState("white");
@@ -38,16 +38,17 @@ function App() {
 
   return (
     <>
-      <Router>
+     {/*</> <Router>*/}
         <Navbar title="TextUtils" mode={mode} AboutText="About" toggleMode={toggleMode} />
         <Alert alert={alert}  />
         <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below mode" mode={mode} />} />
-          </Routes>
+         {/* <Routes>*/ }
+           { /*<Route exact path="/about" element={<About />} />*/}
+            {/*<Route exact path="/" element=*/}
+            <TextForm showAlert={showAlert} heading="Enter the text to analyze below mode" mode={mode} />
+          {/*</Routes>*/}
         </div>
-      </Router>
+      {/*</Router>*/}
     </>
   );
 }
